5840f06371a8db2cbbc9ace27a1cc2e0818fbdaf
Ray Bray <rbray.bray2@gmail.com>
2020-10-23 13:34:51 -0400

    added comments to follow instructions

--- public/db.js
+++ public/db.js
@@ -1,2 +1,4 @@
 // TODO: Wait until solved folder is pushed for the mini project and copy and paste file
-// TODO: Rewatch homework videos
+// TODO: Re-watch homework videos
+// TODO: Link files to index.html and copy and paste script tag from class activities
+// TODO: Jonathan repo's is posted in class activities folder
